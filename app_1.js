164dace7b0bcaf570bdc1954b93dc487771fd200
nola360 <adaramolajr@gmail.com>
2020-02-08 00:17:47 -0500

    factory patterns

--- app_1.js
+++ app_1.js
@@ -1,31 +1,31 @@
 // Constructor function
-function EventObserver() {
-  this.observers = [];
+class EventObserver {
+  constructor() {
+    this.observers = [];
+  }
 
-}
-
-EventObserver.prototype = {
-  subscribe: function (fn) {
+  subscribe(fn) {
     this.observers.push(fn);
     console.log(`You are now subscribed to ${fn.name}`);
+  }
 
-  },
-
-  unsubscribe: function (fn) {
+  unsubscribe(fn) {
     this.observers = this.observers.filter(function (item) {
       if (item !== fn) {
         return item;
       }
     });
     console.log(`You are now unsubscribed from ${fn.name}`)
-  },
+  }
 
-  fire: function () {
+  fire() {
     this.observers.forEach(function (item) {
       item.call()
     })
   }
+
 }
+
 
 const click = new EventObserver();
 
